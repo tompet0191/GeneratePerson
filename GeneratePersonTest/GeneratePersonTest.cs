fda9acda1eceeac7e7e3887528a59c31264ffe4e
tompet0191 <skylines123@gmail.com>
2018-05-24 15:57:25 +0200

    Added unit test for GenerateSocialSecurityNumber

--- GeneratePersonTest/GeneratePersonTest.cs
+++ GeneratePersonTest/GeneratePersonTest.cs
@@ -1,7 +1,9 @@
 ﻿using GeneratePerson;
 using NUnit.Framework;
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace GeneratePersonTest
 {
@@ -37,6 +39,64 @@ namespace GeneratePersonTest
 
                 Assert.That(age, Is.AtLeast(18));
             }
+        }
+
+        [Test]
+        public void ShouldGenerateCorrectPIN()
+        {
+            var PINs = new List<string>();
+
+            
+            for (int x = 0; x < 100000; ++x)
+            {
+                TestPerson.GenerateGender();
+                TestPerson.GenerateBirthDate(false);
+                TestPerson.GenerateSocialSecurityNumber();
+
+                PINs.Add(TestPerson.SocialSecurityNumber);
+            }
+
+            foreach(var pin in PINs)
+            {
+                Assert.That( FixPINformat(pin), Is.True );
+                Assert.That( CheckPIN(pin), Is.True );
+            }
+        }
+
+        private static bool FixPINformat(string str)
+        {
+            var r = new Regex("[^0-9]");
+            var r3 = new Regex(@"^\d{10}$");
+
+            str = r.Replace(str, "");
+
+            return (!r3.IsMatch(str)) ? false : true;
+        }
+
+        private static bool CheckPIN(string str)
+        {
+            int sum = 0;
+            for (int x = 9; x >= 0; x--)
+            {
+                if (x % 2 == 1)
+                    sum += (int)Char.GetNumericValue(str[x]);
+                else
+                {
+
+                    int y = (int)Char.GetNumericValue(str[x]) * 2;
+                    if (y > 9)
+                    {
+                        int[] list = y.ToString().Select(w => (int)Char.GetNumericValue(w)).ToArray();
+
+                        foreach (int i in list)
+                            sum += i;
+                    }
+                    else
+                        sum += (int)Char.GetNumericValue(str[x]) * 2;
+                }
+
+            }
+            return ((sum % 10) == 0);
         }
 
         [TearDown]
